fd01e02962493a0837a10f490d6f465c182f5f2d
NipulK <168514285+NipulK@users.noreply.github.com>
2025-09-10 16:46:53 +0530

    Highlight active nav item on click (in JS file)

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,12 @@ const header = document.querySelector("header")
 window.addEventListener("scroll",function(){
     header.classList.toggle("stricky",window.scroll > 50);
 });
+
+// Highlight active nav item on click
+const navLinks = document.querySelectorAll('.navlist a');
+navLinks.forEach(link => {
+    link.addEventListener('click', function() {
+        navLinks.forEach(l => l.classList.remove('active'));
+        this.classList.add('active');
+    });
+});
